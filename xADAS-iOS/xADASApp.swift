e14f85cc9d3625cfeaf63daff4f281919252d4c2
hmtnvac-cpu <hmtnvac@gmail.com>
2026-09-01 16:16:27 +0700

    Add SwiftUI app entry point

---
+++ xADAS-iOS/xADASApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct xADASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DriveView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
