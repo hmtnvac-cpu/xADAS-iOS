fb358c09af6fa7de96ec9819c6bfbd7d58436903
hmtnvac-cpu <hmtnvac@gmail.com>
2026-09-02 09:10:48 +0700

    Remove iPhone camera source from xADAS configuration

--- xADAS-iOS/Camera/CameraSource.swift
+++ xADAS-iOS/Camera/CameraSource.swift
@@ -1,24 +1,10 @@
 import Foundation
 
-enum CameraSource: String, CaseIterable, Identifiable {
-    case iPhone = "iphone"
-    case seventyMai = "70mai"
-
-    static let storageKey = "xadas.camera.source"
+enum CameraSource {
     static let seventyMaiURLKey = "xadas.camera.70mai.rtspURL"
-
-    var id: String { rawValue }
-
-    var title: String {
-        switch self {
-        case .iPhone: return "iPhone Camera"
-        case .seventyMai: return "70mai A500S (RTSP)"
-        }
-    }
 
     static func registerDefaults() {
         UserDefaults.standard.register(defaults: [
-            storageKey: CameraSource.iPhone.rawValue,
             seventyMaiURLKey: "rtsp://192.168.0.1:554/00000000"
         ])
     }
