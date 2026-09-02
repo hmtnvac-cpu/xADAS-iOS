7affd1e0ac45ae62172a812e539b76463d41e31b
hmtnvac-cpu <hmtnvac@gmail.com>
2026-09-02 09:55:28 +0700

    Use exact working 70mai RTSP URL

--- xADAS-iOS/Camera/CameraSource.swift
+++ xADAS-iOS/Camera/CameraSource.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 enum CameraSource {
-    static let seventyMaiURLKey = "xadas.camera.70mai.rtspURL"
-
-    static func registerDefaults() {
-        UserDefaults.standard.register(defaults: [
-            seventyMaiURLKey: "rtsp://192.168.0.1:554/00000000"
-        ])
-    }
+    static let seventyMaiURL = "rtsp://192.168.0.1/00000000"
 }
